f86d393a2b42f2888ba2c4fd39ec47f4b0f80846
Garet Claborn <stealthpaladin@gmail.com>
2014-04-14 13:23:21 -0500

    Update map.php

--- project/map.php
+++ project/map.php
@@ -1,12 +1,24 @@
-<?
+<?php
 
+require_once( __DIR__.'/core.php');
+require_once( __DIR__.'/support/datasets/compositions.php');
 
-require_once( '../../approach/core.php');
-
-$DefaultRoot = 9;
+/* Set up environment */
+global $ApproachServiceCall;
 global $ActiveComposition;
 global $ActiveCompositionContext;
-global $ApproachServiceCall;
+
+$ActiveComposition = array();
+$ActiveCompositionContext['path'] = '';
+$ActiveCompositionContext['id']= array();
+$ActiveCompositionContext['type']= array();
+$ActiveCompositionContext['typeid']= array();
+
+$DefaultRoot = 0;
+
+if(!$ApproachServiceCall){  $ActiveComposition = RouteFromURL($_SERVER['REQUEST_URI']);  }
+
+
 
 function RouteFromURL($url, $silent=false)
 {
@@ -15,128 +27,83 @@ function RouteFromURL($url, $silent=false)
     global $ActiveComposition;
     global $ActiveCompositionContext;
 
-    /* Set up environment */
-
-    $ActiveComposition = array();
-    $ActiveCompositionContext['path'] = '';
-    $ActiveCompositionContext['id']= array();
-    $ActiveCompositionContext['type']= array();
-    $ActiveCompositionContext['typeid']= array();
-    
-    $options = array();
-
     if(!isset($url)) $url = $_SERVER['REQUEST_URI'];
-    $url = urldecode($url);
+    $AppPath = array('home');
+    $AppPath=array_merge($AppPath, ArrayFromURI($url));
 
+
+    /*  Root Level & Type Detection  */
+
+    $RootSearch=new compositions('compositions');
+    $RootSearch->data['id']=$DefaultRoot;
+
+    
+    $ActiveCompositionContext['path']=ResolveComposition($RootSearch,$AppPath);
+    $ActiveCompositionContext['data']=$ActiveCompositionContext['traversed'][count($ActiveCompositionContext['traversed'])-1];
+
+    $ActiveCompositionContext['entry'] = $AppPath;
+//    $ActiveCompositionContext['data'] = $RootSearch->data;
+    $ActiveCompositionContext['path'] .= '/compose.php';
+    
+    $ActiveComposition = new Composition();
+    $ActiveCompositionContext['self'] = $RootSearch;  //Database Values for this node
+
+    array_shift($ActiveCompositionContext['traversed']);
+    require_once(__DIR__ . '/'.$ActiveCompositionContext['path']);  
+
+    $ActiveComposition->publish($silent);
+    $ActiveCompositionContext['self'] = $ActiveComposition;  //Application instantiated by running node values throw chained scopes
+
+    return $ActiveComposition;
+}
+
+function ArrayFromURI(&$uri)
+{
+    $result=array();
+    $uri = urldecode($uri);
     $exts=array('.aspx','.asp','.jsp','.php','.html','.htm','.rhtml','.py','.cfm','.cfml', '.cpp', '.c', '.ruby','.dll', '.asm');
-    $url = str_replace($exts, '', $url);
-    $AppPath = explode('/',$url);
+    $uri = str_replace($exts, '', $uri);
+    $result = explode('/',$uri);
 
-    for($i=0, $L=count($AppPath); $i<$L; $i++)
+    for($i=0, $L=count($result); $i<$L; $i++)
     {
-        if($AppPath[$i] == '' || empty($AppPath[$i])){ unset($AppPath[$i]); continue; }
-        else $AppPath[$i] = strtolower($AppPath[$i]);
+        if($result[$i] == '' || empty($result[$i])){ unset($result[$i]); continue; }
+        else $result[$i] = strtolower($result[$i]);
     }
 
-    $AppPath = array_values($AppPath);
-
-    /* URI has been transformed to an array structure */
-    /*  Root Level Detection  */
-    if($url == '/' ||$url == '/home' || $url == '/index' || $url == '/default' )
-    {
-        $options=array();
-        //$options['condition']= '[id] = 1 AND [root] = 1';
-        $options['command']= 'SELECT * ';
-        
-        $Composition = LoadObject('compositions', $options);
-
-        $ActiveCompositionContext['path'] .= 'compose.php';        
-        //$ActiveCompositionContext['data'] = $Composition->data;
-
-        $Composing = new Composition();
-        
-        require_once(__DIR__ . '/'.$ActiveCompositionContext['path']);
-        $Composing->publish($silent);
-        
-        $ActiveComposition = $Composing;
-        $ActiveCompositionContext['self'] = $ActiveComposition;
-        return $Composing;  //Done
-    }
-
-    /*  Dynamic Nested Compositions */
-    
-    
-    /*   Get Root Type   */
-
-    $options['method']= 'WHERE alias LIKE \'' . $AppPath[0] . '\' AND (parent = 1 OR parent = '.$DefaultRoot.')';
-    $options['condition']= 'ORDER BY self LIMIT 1';
-    
-    $RootSearch = LoadObject('compositions', $options);
-
-    //print_r($RootSearch);
-    if(count($RootSearch) < 1 )
-    {
-        exit("FAILED TO ROUTE Composition: PRIMARY TYPE SEARCH FAILURE. ");
-    }
-    else
-    {
-        $options['method']= 'WHERE id='.$RootSearch->data['scope'];
-        $options['condition']= 'ORDER BY id LIMIT 1';
-        $Type = LoadObject('types', $options);
-    }
-
-    $ActiveCompositionContext['id'][] = $RootSearch->data['id'];
-    $ActiveCompositionContext['type'][]=$Type->data['Name'];
-    $ActiveCompositionContext['typeid'][]= $Type->data['id'];
-    $ActiveCompositionContext['path'] .=  $Type->data['Name'] . '/';
-    
-    
-    /*  Get Root Composition For Next Type  */
-  for($i=1, $L=count($AppPath); $i<$L; $i++)
-  {
-      /*    Match Currrent Type Root Composition    */
-        $options['method']= 'WHERE alias LIKE \'' . $AppPath[$i] . '\' AND parent = '.$RootSearch->data['id'];
-        $options['condition']= 'ORDER BY self LIMIT 1';
-        
-        $RootSearch = LoadObject('compositions', $options);
-
-        if(count($RootSearch) < 1 )
-        {
-            exit("FAILED TO ROUTE Composition: PRIMARY TYPE SEARCH FAILURE. ");
-        }
-        else
-        {
-            $options['method']= 'WHERE id='.$RootSearch->data['scope'];
-            $options['condition']= 'ORDER BY id LIMIT 1';
-            $Type = LoadObject('types', $options);
-        }
-        
-        $ActiveCompositionContext['id'][] = $RootSearch->data['id'];
-        $ActiveCompositionContext['type'][]=$Type->data['Name'];
-        $ActiveCompositionContext['typeid'][]= $Type->data['id'];
-        $ActiveCompositionContext['traversed'][]=$RootSearch->data;
-        
-        if($RootSearch->data['root']==1) $ActiveCompositionContext['path'] .= $Type->data['Name'] .'/';
-        else $ActiveCompositionContext['path'] .= 'compose.php';
-  }
-
-  $ActiveCompositionContext['apppath'] = $AppPath;
-  $ActiveCompositionContext['data'] = $RootSearch->data;
-  if($RootSearch->data['root']==1 || count($AppPath) ==1) $ActiveCompositionContext['path'] .= 'compose.php';
-
-  $Composing = new Composition();
-  $ActiveCompositionContext['self'] = $RootSearch;
-  require_once(__DIR__ . '/'.$ActiveCompositionContext['path']);
-  $Composing->publish($silent);
-
-  $ActiveComposition = $Composing;
-  $ActiveCompositionContext['self'] = $ActiveComposition;
-  return $Composing;
-
- // $RoutedComposition = new Composition();
+    return  array_values($result);
 }
 
 
-if(!$ApproachServiceCall){  $ActiveComposition = RouteFromURL($_SERVER['REQUEST_URI']);  }
+function ResolveComposition($RootSearch,$PathList)
+{
+    global $ActiveCompositionContext;
+    $options['method']= 'WHERE `alias` LIKE \'' . $PathList[0] . '\' AND `parent` = '.$RootSearch->data['id'].' ';
+    $options['condition']= 'ORDER BY self LIMIT 1';
+//    $options['debug']=true;
 
+    $RootSearch = LoadObject('compositions', $options);
+
+    if(!isset($RootSearch->data['type']) )
+    {
+        $options['method']= 'WHERE `title` LIKE \'' . $PathList[0].'\'';
+        $options['condition']= 'LIMIT 1';
+        
+        $RootSearch = LoadObject('compositions', $options);
+
+        if(!isset($RootSearch->data['type']) ) exit('Failed To Route Composition: TYPECAST FAILURE.');
+    }
+    
+    $options['method']= 'WHERE id='.$RootSearch->data['type'];
+    $options['condition']= 'ORDER BY id LIMIT 1';
+    $Type = LoadObject('types', $options);
+        
+    $ActiveCompositionContext['id'][]=$RootSearch->data['id'];
+    $ActiveCompositionContext['type'][]=$Type->data['name'];
+    $ActiveCompositionContext['typeid'][]= $Type->data['id'];
+    $ActiveCompositionContext['traversed'][]=$RootSearch->data;
+    
+    if(count($PathList)<=1) return $Type->data['name'];
+    else return $Type->data['name'] .'/'.ResolveComposition($RootSearch,array_slice($PathList, 1));
+}
 ?>
